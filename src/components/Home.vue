46af80659c125585dc2a331d5dbdef80fd079348
Jayven Couch <jayvencouch11@gmail.com>
2022-03-01 05:14:30 -1000

    init

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="staticContainer">
     <div class="column">
-      <div class="staticExplore col-md-4">
+      <div class="staticExplore col-md-5">
         <div class="exploreContainer">
           <h1 class="slogan">Explore</h1>
           <div class="moreDetails">
@@ -19,17 +19,17 @@
               <p class="bodyDescription">
                 Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur?
               </p>
-              <a href="#" class="readMoreButton">Read More</a>
+              <a class="readMoreButton">Read More</a>
             </div>
           </div>
         </div>
-        <div class="socialIconLayout col-sm-8">
+        <div class="socialIconLayout">
           <div class="socialIcons">
             <a class="socials">
-              <img src="../assets/Facebook.svg" alt="" width="55"/>
+              <img src="../assets/Facebook.svg" alt="" width="50"/>
             </a>
             <a class="socials">
-              <img src="../assets/Instagram.svg" alt="" width="60"/>
+              <img src="../assets/Instagram.svg" alt="" width="50"/>
             </a>
           </div>
         </div>
