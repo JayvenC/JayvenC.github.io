490f804f0fc534ce387737f4aa4b1ddccbee177e
Jayven Couch <jayvencouch11@gmail.com>
2022-03-01 03:24:38 -1000

    changes

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,7 +1,40 @@
 <template>
-<img src="../assets/image.png" class="image" alt="">
-  <div class="hello container">
-    <h1 class='slogan'>Explore</h1>
+  <div class="staticContainer">
+    <div class="column">
+      <div class="staticExplore col-md-4">
+        <div class="exploreContainer">
+          <h1 class="slogan">Explore</h1>
+          <div class="moreDetails">
+          <div class="detailsClick">
+            <img src="../assets/Plus.svg" 
+            class='plusButton' alt="" />
+            <div>More Details</div>
+            </div>
+          </div>
+          <div class="detailsPopup">
+            <div class="bodyOfDetails">
+              <div class="closeDetails fa-solid fa-xmark"></div>
+              <h3 class="detailsTitle">Explore</h3>
+              <div class="divider"></div>
+              <p class="bodyDescription">
+                Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur?
+              </p>
+              <a href="#" class="readMoreButton">Read More</a>
+            </div>
+          </div>
+        </div>
+        <div class="socialIconLayout col-sm-8">
+          <div class="socialIcons">
+            <a class="socials">
+              <img src="../assets/Facebook.svg" alt="" width="55"/>
+            </a>
+            <a class="socials">
+              <img src="../assets/Instagram.svg" alt="" width="60"/>
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -11,7 +44,6 @@ export default {
 }
 
 </script>
-
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 </style>
--- src/main.js
+++ src/main.js
@@ -4,3 +4,25 @@ import { createApp } from 'vue'
 import App from './App.vue'
 
 createApp(App).mount('#app')
+
+let clickButton = document.querySelector(".detailsClick")
+let popupWindow = document.querySelector(".detailsPopup")
+let close = document.querySelector(".closeDetails")
+let detailsBody = document.querySelector(".bodyOfDetails")
+let exploreTitle = document.querySelector(".slogan")
+
+clickButton.addEventListener("click", function () {
+    popupWindow.classList.remove("window-exit");
+    popupWindow.classList.add("window-add");
+    detailsBody.classList.remove("details-exit");
+    detailsBody.classList.add("details-show");
+    exploreTitle.classList.add("title-remove")
+});
+
+close.addEventListener("click", function () {
+    popupWindow.classList.remove("window-add");
+    popupWindow.classList.add("window-exit");
+    detailsBody.classList.remove("details-show");
+    detailsBody.classList.add("details-exit");
+    exploreTitle.classList.remove("title-remove");
+});
